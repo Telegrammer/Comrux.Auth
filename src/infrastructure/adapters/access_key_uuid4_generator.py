0b39a9e9f23bceeb6ac765643b51a0f8d1fb76aa
Telegrammer <bihug95@gmail.com>
2025-08-26 16:28:57 +0300

    Update id generator to make correct entity ids

--- src/infrastructure/adapters/access_key_uuid4_generator.py
+++ src/infrastructure/adapters/access_key_uuid4_generator.py
@@ -1,4 +1,3 @@
-from domain.value_objects import Uuid4
 from domain import AccessKeyId
 import uuid
 from domain.ports import UserIdGenerator
@@ -8,4 +7,4 @@ __all__ = ["AccessKeyUuid4Generator"]
 
 class AccessKeyUuid4Generator(UserIdGenerator):
     def __call__(self) -> AccessKeyId:
-        return Uuid4(str(uuid.uuid4()))
+        return AccessKeyId(str(uuid.uuid4()))
--- src/infrastructure/adapters/user_uuid4_generator.py
+++ src/infrastructure/adapters/user_uuid4_generator.py
@@ -1,4 +1,4 @@
-from domain.value_objects import Uuid4, Id
+from domain import UserId
 import uuid
 from domain.ports import UserIdGenerator
 
@@ -6,5 +6,5 @@ __all__ = ["UserUuid4Generator"]
 
 
 class UserUuid4Generator(UserIdGenerator):
-    def __call__(self, *args, **kwargs) -> Id:
-        return Uuid4(str(uuid.uuid4()))
+    def __call__(self, *args, **kwargs) -> UserId:
+        return UserId(str(uuid.uuid4()))
