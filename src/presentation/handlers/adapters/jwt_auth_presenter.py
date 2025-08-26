9fc803f7c4bf20b16145555d56af497e1487bbe0
Telegrammer <bihug95@gmail.com>
2025-08-26 16:31:58 +0300

    Add functionality to jwt auth info presenter

--- src/presentation/handlers/adapters/jwt_auth_presenter.py
+++ src/presentation/handlers/adapters/jwt_auth_presenter.py
@@ -1,51 +1,73 @@
 __all__ = ["JwtAuthInfoPresenter"]
 
-from datetime import datetime, timedelta
-from typing import TypedDict, Any
 
-from application.usecases.login_user import LoginUserResponse
-from presentation.handlers.ports import AuthInfoPresenter
-from presentation.models import JwtInfo
+from datetime import datetime, timedelta
+from typing import Any, Optional
 
 import jwt
+
+from presentation.handlers.ports import AuthInfoPresenter
+from presentation.models import JwtInfo, AuthInfo
 
 
 class JwtAuthInfoPresenter(AuthInfoPresenter):
     def __init__(
         self,
         secret_key: str,
+        public_key: str,
         algorithm: str,
         access_token_expiration_time: timedelta,
     ):
         self._secret_key = secret_key
+        self._public_key = public_key
         self._algorithm = algorithm
         self._access_token_expiration_time = access_token_expiration_time
 
-    def _create_jwt_tokens(self, payloads: list[dict[str, Any]]) -> tuple[str]:
-        return tuple(
-            jwt.encode(payload=payload, key=self._secret_key, algorithm=self._algorithm)
-            for payload in payloads
-        )
+    def _encode(self, payload: dict[str, Any]) -> str:
+        return jwt.encode(payload, self._secret_key, algorithm=self._algorithm)
 
-    def present(self, usecase_response: LoginUserResponse) -> JwtInfo:
-        iat: datetime = usecase_response.pop("created_at")
-        access_exp: datetime = iat + self._access_token_expiration_time
-        refresh_exp: datetime = usecase_response.pop("expire_at")
+    def _decode(self, credentials: bytes) -> dict[str, Any]:
+        return jwt.decode(jwt=credentials, key=self._public_key, algorithms=self._algorithm)
 
-        base_payload = {
-            **usecase_response,
-            "sub": usecase_response.pop("key_id"),
+    def present(self, usecase_response: AuthInfo) -> JwtInfo:
+
+        auth_info: dict = usecase_response.model_dump()
+
+        created_at: datetime = auth_info.pop("created_at")
+        access_exp: datetime = created_at + self._access_token_expiration_time
+        refresh_exp: datetime | None = auth_info.pop("expire_at")
+
+        base_payload: dict[str, Any] = {
+            "sub": auth_info.pop("key_id"),
+            **auth_info,
         }
 
-        access_payload = {**base_payload, "exp": access_exp, "iat": iat}
-        refresh_payload = {**base_payload, "exp": refresh_exp, "iat": iat}
+        access_payload = {**base_payload, "exp": access_exp, "iat": created_at, "type": "access"}
+        access_token = self._encode(access_payload)
 
-        access_token, refresh_token = self._create_jwt_tokens(
-            [access_payload, refresh_payload]
-        )
+        refresh_token: str | None = None
+        if refresh_exp is not None:
+            refresh_payload = {**base_payload, "exp": refresh_exp, "iat": created_at, "type": "refresh"}
+            refresh_token = self._encode(refresh_payload)
 
         return JwtInfo(
             access_token=access_token,
             refresh_token=refresh_token,
             token_type="Bearer",
+        )
+
+    def to_auth_info(self, credentials: bytes, required_type: str) -> AuthInfo | None:
+        try:
+            payload: dict[str, Any] = self._decode(credentials)
+        except jwt.exceptions.ExpiredSignatureError:
+            return None
+    
+        if payload.get("type", None) != required_type:
+            return None
+
+        return AuthInfo(
+            key_id=payload["sub"],
+            user_id=payload["user_id"],
+            created_at=payload["iat"],
+            expire_at=payload["exp"],
         )
