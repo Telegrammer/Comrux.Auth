ccffbe7989cb3d3b2f741c9798ec9406d6c88231
Telegrammer <bihug95@gmail.com>
2025-08-26 16:33:06 +0300

    Refactor presentation models

--- src/presentation/models/auth.py
+++ src/presentation/models/auth.py
@@ -1,17 +1,24 @@
 __all__ = ["AuthInfo", "JwtInfo", "SessionInfo"]
 
 
+from typing import TypedDict
 from pydantic import BaseModel
+from datetime import datetime
 
 
-class AuthInfo(BaseModel): ...
+class AuthInfo(BaseModel):
+    key_id: str
+    user_id: str
+
+    created_at: datetime
+    expire_at: datetime | None = None
 
 
-class JwtInfo(AuthInfo):
+class JwtInfo(BaseModel):
     access_token: str
-    refresh_token: str
+    refresh_token: str | None
     token_type: str
 
 
-class SessionInfo(AuthInfo):
+class SessionInfo(BaseModel):
     session_id: str
