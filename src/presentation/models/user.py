09993aa9bf2948cea03714bd7051c1bf963f5bc1
Telegrammer <bihug95@gmail.com>
2025-09-08 16:09:40 +0300

    Make polymorph user login models

--- src/presentation/models/user.py
+++ src/presentation/models/user.py
@@ -1,19 +1,25 @@
 from pydantic import BaseModel, EmailStr
 from pydantic import UUID4
 
-__all__ = ["UserCreate", "UserRead", "UserLogin"]
+__all__ = ["UserCreate", "UserRead", "UserLogin", "PasswordUserLogin"]
+
 
 class BaseUser(BaseModel):
     email: EmailStr
     phone: str
-        
+
 
 class UserCreate(BaseUser):
     password: str
 
+
 class UserRead(BaseUser):
     user_id: UUID4
 
-class UserLogin(BaseModel):
+
+class UserLogin(BaseModel): ...
+
+
+class PasswordUserLogin(UserLogin):
     email: EmailStr
     password: str
