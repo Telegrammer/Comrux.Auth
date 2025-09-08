73f1d2c8115e754a69b522364aa3195447d84895
Telegrammer <bihug95@gmail.com>
2025-09-08 16:03:58 +0300

    Make redis adapter for PipeLine and Redis objects

---
+++ src/infrastructure/adapters/redis_adapter.py
@@ -0,0 +1,21 @@
+__all__ = ["RedisAdapter"]
+
+
+from utils import decode
+from redis.asyncio import Redis
+import makefun
+
+
+class RedisAdapter:
+
+    def __init__(self, original_client: Redis):
+        self._client: Redis = original_client
+        self.hset = self._client.hset
+        self.expire = self._client.expire
+
+    @decode
+    @makefun.wraps(Redis.hgetall)
+    async def hgetall(self, *args, **kwargs):
+        async with self._client.pipeline(transaction=False) as pipe:
+            pipe.hgetall(*args, **kwargs)
+            return await pipe.execute()
