263af27b5bd9fe9badda1c707e2d546c385ebc95
Telegrammer <bihug95@gmail.com>
2025-09-08 16:04:55 +0300

    Overload convertion to domain for access key mapper

--- src/infrastructure/adapters/mappers/access_key.py
+++ src/infrastructure/adapters/mappers/access_key.py
@@ -6,36 +6,50 @@ from datetime import datetime
 
 from domain import AccessKey, AccessKeyId, UserId
 from domain.value_objects import PassedDatetime, FutureDatetime
+from functools import singledispatchmethod
 
 
 from application.ports.mappers import AccessKeyMapper, MappingError
 from infrastructure.models import AccessKey as DbAccessKey
 from infrastructure.exceptions import create_error_aware_decorator
+from pydantic import ValidationError
 
+type_mismatch_aware = create_error_aware_decorator(
+    {frozenset({TypeError, AttributeError, ValidationError}): MappingError}
+)
 
-type_mismatch_aware = create_error_aware_decorator({
-    frozenset({TypeError, AttributeError}): MappingError
-})
 
 class RedisAccessKeyMapper(AccessKeyMapper[DbAccessKey]):
 
-
-    @type_mismatch_aware
     def to_dto(self, entity: AccessKey) -> DbAccessKey:
-        return DbAccessKey(
-            id_=f"access_key:{entity.id_}",
-            user_id=entity.user_id,
-            created_at=entity.created_at.isoformat(),
-            expire_at=entity.expire_at.isoformat(),
-        )
+        try:
+            return DbAccessKey(
+                id_=f"access_key:{entity.id_}",
+                user_id=entity.user_id,
+                created_at=entity.created_at.isoformat(),
+                expire_at=entity.expire_at.isoformat(),
+            )
+        except (TypeError, AttributeError, ValidationError, ValueError):
+            raise MappingError("Cannot complete operation")
 
-    @type_mismatch_aware
-    def to_domain(self, dto: DbAccessKey) -> AccessKey:
-        issued_at: datetime = datetime.fromisoformat(dto.created_at)
-        expire_at: datetime = datetime.fromisoformat(dto.expire_at)
-        return AccessKey(
-            id_=AccessKeyId(dto.id_),
-            user_id=UserId(dto.user_id),
-            created_at=PassedDatetime(issued_at, issued_at),
-            expire_at=FutureDatetime(expire_at, issued_at),
-        )
+    @singledispatchmethod
+    def to_domain(self, dto) -> AccessKey:
+        raise MappingError(f"Unsupported dto type: {type(dto)}")
+
+    @to_domain.register
+    def _(self, dto: DbAccessKey) -> AccessKey:
+        try:
+            issued_at: datetime = datetime.fromisoformat(dto.created_at)
+            expire_at: datetime = datetime.fromisoformat(dto.expire_at)
+            return AccessKey(
+                id_=AccessKeyId(dto.id_),
+                user_id=UserId(dto.user_id),
+                created_at=PassedDatetime(issued_at, issued_at),
+                expire_at=FutureDatetime(expire_at, issued_at),
+            )
+        except (TypeError, AttributeError, ValidationError, ValueError):
+            raise MappingError("Cannot complete operation")
+
+    @to_domain.register
+    def _(self, dto: dict) -> AccessKey:
+        return self.to_domain(DbAccessKey.model_validate(dto))
