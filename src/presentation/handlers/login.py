a476f10af781ffc27cb6dff928ca37160b38db7b
Telegrammer <bihug95@gmail.com>
2025-09-08 16:08:54 +0300

    Replace usecase to his factory

--- src/presentation/handlers/login.py
+++ src/presentation/handlers/login.py
@@ -1,25 +1,35 @@
 __all__ = ["LoginHandler"]
 
+
+from application.ports import UnitOfWork
+
 from application.usecases import (
-    PasswordLoginUserRequest,
-    PasswordLoginUsecase,
     LoginUserResponse,
 )
 
-from presentation.handlers.ports import AuthInfoPresenter
 from presentation.models import UserLogin, AuthInfo
+
+from .ports import AuthInfoPresenter, LoginUsecaseFactory
 
 
 class LoginHandler:
 
-    def __init__(self, login_usecase: PasswordLoginUsecase, auth_presenter: AuthInfoPresenter):
-        self._login_usecase = login_usecase
+    def __init__(
+        self,
+        usecase_factory: LoginUsecaseFactory,
+        auth_presenter: AuthInfoPresenter,
+        unit_of_work: UnitOfWork,
+    ):
+        self._usecase_factory = usecase_factory
         self._auth_presenter = auth_presenter
+        self._unit_of_work = unit_of_work
 
     async def __call__(self, request: UserLogin) -> AuthInfo:
-        response: LoginUserResponse = await self._login_usecase(
-            PasswordLoginUserRequest.from_primitives(**request.model_dump())
-        )
+
+        usecase, usecase_request = self._usecase_factory(request)
+
+        async with self._unit_of_work:
+            response: LoginUserResponse = await usecase(usecase_request)
 
         auth_info: AuthInfo = AuthInfo(**response)
         return self._auth_presenter.present(auth_info)
