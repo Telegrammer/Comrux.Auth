d878a599c7294df09da7d8c938c9340323c93124
Telegrammer <bihug95@gmail.com>
2025-09-09 16:15:28 +0300

    Build delete method for access key gateway

--- src/infrastructure/adapters/gateways/access_key.py
+++ src/infrastructure/adapters/gateways/access_key.py
@@ -17,6 +17,7 @@ network_error_aware = create_error_aware_decorator(
 )
 
 
+# TODO: inject naming_convection for keys to remove repeating declaration
 class RedisAccessKeyCommandGateway:
 
     def __init__(self, client: Redis, mapper: AccessKeyMapper):
@@ -26,17 +27,30 @@ class RedisAccessKeyCommandGateway:
     @network_error_aware("Cannot add access key: there is no place to add him")
     async def add(self, access_key: AccessKey) -> None:
         db_access_key: DbAccessKey = self._mapper.to_dto(access_key)
+
+        user_index: str = f"user_access_keys:{db_access_key.user_id}"
+        key_id: str = f"access_key:{db_access_key.id_}"
+
+        ttl: int = int((access_key.expire_at - access_key.created_at).total_seconds())
+
         await self._client.hset(
             db_access_key.id_,
             mapping=db_access_key.model_dump(exclude={"id_"}),
         )
         await self._client.expire(
             db_access_key.id_,
-            int((access_key.expire_at - access_key.created_at).total_seconds()),
+            ttl,
         )
+        await self._client.sadd(user_index, key_id)
+        await self._client.expire(user_index, ttl)
 
     async def delete(self, access_key: AccessKey) -> None:
-        raise NotImplementedError
+        db_access_key: DbAccessKey = self._mapper.to_dto(access_key)
+        user_index: str = f"user_access_keys:{db_access_key.user_id}"
+        key_id: str = f"access_key:{db_access_key.id_}"
+
+        await self._client.delete(db_access_key.id_)
+        await self._client.srem(user_index, key_id)
 
 
 class RedisAccessKeyQueryGateway:
@@ -49,7 +63,7 @@ class RedisAccessKeyQueryGateway:
         "Cannot find access key: access keys are lost. Try again later"
     )
     async def by_id(self, access_key_id: AccessKeyId) -> AccessKey:
-        
+
         found_key: dict[str, str] | None = await self._client.hgetall(
             f"access_key:{access_key_id}"
         )
@@ -57,7 +71,9 @@ class RedisAccessKeyQueryGateway:
         if not found_key:
             raise AccessKeyNotFound("Access key with given id does not exists")
 
-        return self._mapper.to_domain(DbAccessKey.model_validate({"id_": access_key_id, **found_key}))
+        return self._mapper.to_domain(
+            DbAccessKey.model_validate({"id_": access_key_id, **found_key})
+        )
 
     async def by_user(self, user: User) -> Sequence[AccessKey] | None:
         raise NotImplementedError
