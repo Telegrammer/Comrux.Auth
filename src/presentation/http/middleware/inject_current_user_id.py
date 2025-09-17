baa9a03604b73849f3adca29992f9059ee22eab6
Telegrammer <bihug95@gmail.com>
2025-09-17 14:15:30 +0300

    Hide middleware side operations via composition and decorator

--- src/presentation/http/middleware/inject_current_user_id.py
+++ src/presentation/http/middleware/inject_current_user_id.py
@@ -1,11 +1,10 @@
 __all__ = ["InjectCurrentUserIdMiddleware"]
 
 
-from typing import Awaitable, Callable, Optional, TypeVar
+from typing import Awaitable, Callable, Optional, Type
 
-from dishka import AsyncContainer
-from fastapi import FastAPI, Request
-from starlette.datastructures import State
+from dishka import AsyncContainer, DependencyKey
+from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import Response
 from starlette.types import ASGIApp
@@ -13,12 +12,10 @@ from starlette.types import ASGIApp
 
 
 from domain import UserId
-from application.services import CurrentUserService
 from presentation.handlers.adapters.presenters import JwtAuthInfoPresenter
+from presentation.handlers.ports.presenters import AuthInfoPresenter
 from presentation.models import AuthInfo
-
-
-
+from .update_context import update_context
 
 class InjectCurrentUserIdMiddleware(BaseHTTPMiddleware):
 
@@ -28,31 +25,26 @@ class InjectCurrentUserIdMiddleware(BaseHTTPMiddleware):
         app: ASGIApp,
         dispatch: Optional[Callable[[Request, Callable[[Request], Awaitable[Response]]], Awaitable[Response]]] = None,
     ) -> None:
-        super().__init__(app, dispatch=dispatch)
+        self._presenter: AuthInfoPresenter = None
+        super().__init__(app, dispatch=update_context(self.dispatch))
     
 
-    async def dispatch(self, request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
+    async def dispatch(self, request: Request) -> dict[DependencyKey, object | Type]:
 
         #TODO: encapsulate HTTPCredentials into another object 
         auth_header: str = request.headers.get("Authorization")
 
-        if not auth_header:
-            return await call_next(request)
-        
-        if not auth_header.startswith("Bearer"):
-            return await call_next(request)
+        if not (auth_header and auth_header.startswith("Bearer")):
+            return {}
         
         credentials: bytes = auth_header.replace("Bearer ", "").encode()
 
-        app_container: AsyncContainer = request.app.state.dishka_container
-        presenter: JwtAuthInfoPresenter = await app_container.get(JwtAuthInfoPresenter)
+        if not self._presenter:
+            app_container: AsyncContainer = request.app.state.dishka_container
+            self._presenter = await app_container.get(AuthInfoPresenter)
 
-        auth_info: AuthInfo = presenter.to_auth_info(credentials, "any")
+        auth_info: AuthInfo = self._presenter.to_auth_info(credentials, "any")
         if not auth_info.user_id:
-            return await call_next(request)
+            return {}
 
-        user_id: UserId = UserId(auth_info.user_id)
-
-        async with app_container(context={UserId: user_id}) as request_container:
-            request.state.dishka_container = request_container
-            return await call_next(request)
+        return {UserId: UserId(auth_info.user_id)}
