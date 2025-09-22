b640e586192cd5cf5b1519f546ea3be5c1b42598
Telegrammer <bihug95@gmail.com>
2025-09-22 10:13:26 +0300

    Identify new core object for inner message handling

--- src/main.py
+++ src/main.py
@@ -2,10 +2,13 @@ from contextlib import asynccontextmanager
 from dishka import make_async_container, AsyncContainer
 
 import uvicorn
+import dishka.integrations.fastapi as fastapi_integration
+from dishka.integrations.fastapi import FromDishka, inject
+import dishka.integrations.faststream as faststream_integration
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from dishka.integrations.fastapi import setup_dishka, FromDishka, inject
-from functools import partial
+from faststream import FastStream
+from faststream.redis import RedisBroker
 
 from setup import (
     Settings,
@@ -21,36 +24,41 @@ from domain import UserService
 from presentation.http.controllers.user import user_router
 from presentation.http.middleware import InjectCurrentUserIdMiddleware
 
+from infrastructure.subscribers import user_sub_router
+
 origins = ["http://localhost:8000", "http://127.0.0.1:3000", "http://localhost:3000"]
 
+broker = RedisBroker(str(settings.cache.url))
 
 container: AsyncContainer = make_async_container(
     DatabaseProvider(),
     DomainProvider(),
     ApplicationProvider(),
     PresentationProvider(),
-    context={Settings: settings},
+    context={Settings: settings, RedisBroker: broker},
 )
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+
     async with container() as app_state:
         app.state.container = app_state
-
+        await broker.start()
     yield
 
     db_helper = await container.get(DatabaseHelper)
     await db_helper.dispose()
+    await broker.stop()
+
+
+broker.include_router(user_sub_router)
+faststream_integration.setup_dishka(container, FastStream(broker))
 
 
 auth_app = FastAPI(lifespan=lifespan)
-
 auth_app.include_router(user_router)
-auth_app.add_middleware(
-    InjectCurrentUserIdMiddleware
-)
-
+auth_app.add_middleware(InjectCurrentUserIdMiddleware)
 auth_app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -59,7 +67,8 @@ auth_app.add_middleware(
     allow_headers=["*"],
 )
 
-setup_dishka(container=container, app=auth_app)
+fastapi_integration.setup_dishka(container=container, app=auth_app)
+
 
 @auth_app.get("/")
 @inject
