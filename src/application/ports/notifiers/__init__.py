cc96a37e836f8bef31a8d359e576c7d188e36a95
Telegrammer <bihug95@gmail.com>
2025-09-22 09:59:58 +0300

    Insert new type of port for inner communication: notifiers

---
+++ src/application/ports/notifiers/__init__.py
@@ -0,0 +1,4 @@
+from .sensetive_data_change import (
+    SensetiveDataChangeNotifier,
+    SensetiveDataChangePayload,
+)
---
+++ src/application/ports/notifiers/sensetive_data_change.py
@@ -0,0 +1,22 @@
+from typing import Protocol
+from abc import abstractmethod
+from dataclasses import dataclass
+
+from domain import UserId
+from datetime import datetime
+
+
+@dataclass
+class SensetiveDataChangePayload:
+
+    user_id: UserId
+    changed_data_types: tuple[str]
+
+    occured: datetime
+
+
+class SensetiveDataChangeNotifier(Protocol):
+
+    @abstractmethod
+    async def notify(self, event_info: SensetiveDataChangePayload) -> None:
+        raise NotImplementedError
