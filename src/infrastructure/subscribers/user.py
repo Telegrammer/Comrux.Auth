fdb82b5f347ca8f1793e359235b68c0e906af037
Telegrammer <bihug95@gmail.com>
2025-09-23 16:18:07 +0300

    Remove service locator from subsctiber

--- src/infrastructure/subscribers/user.py
+++ src/infrastructure/subscribers/user.py
@@ -1,31 +1,31 @@
 __all__ = ["sub_router"]
 
 
-from pydantic import BaseModel
-from datetime import datetime
-from dishka import AsyncContainer
 from dishka.integrations.faststream import FromDishka, inject
+from faststream.redis import RedisRouter, StreamSub
 
-from faststream.redis import RedisRouter
-from faststream import BaseMiddleware
-
-
-from domain import UserId
 from application import UnitOfWork, LogoutAllUsecase
+from utils import generate_consumer_id
+
+from .models.user import UserSensetiveDataChange
 
 sub_router = RedisRouter()
 
-class UserSensetiveDataChange(BaseModel):
-    user_id: str
-    changed_fields: str
-    occured: datetime
 
-
-@sub_router.subscriber(stream="user:sensetive_data_changed")
+@sub_router.subscriber(
+    stream=StreamSub(
+        "user:sensetive_data_changed",
+        group="user-group",
+        consumer=generate_consumer_id(),
+        last_id=">",
+    )
+)
 @inject
-async def logout_all(container: FromDishka[AsyncContainer], message: UserSensetiveDataChange):
-    async with container.parent_container(context={UserId: UserId(message.user_id)}) as request_container:
-        uow: UnitOfWork = await request_container.get(UnitOfWork)
-        usecase: LogoutAllUsecase = await request_container.get(LogoutAllUsecase)
-        async with uow:
-            await usecase()
+async def logout_all(
+    uow: FromDishka[UnitOfWork],
+    usecase: FromDishka[LogoutAllUsecase], 
+    message: UserSensetiveDataChange
+):
+    async with uow:
+        await usecase()
+    return True
