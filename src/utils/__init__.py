2b0b7f6c23c7748a89e651e95c30e0c3d854db35
Telegrammer <bihug95@gmail.com>
2025-09-23 16:20:34 +0300

    Displace merge context function from presentation to utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,4 @@
 from .bytes_structs_converter import decode
 from .case_converter import camel_case_to_snake_case
+from .consumer_id_generator import generate_consumer_id
+from .merge_context import merge_context
