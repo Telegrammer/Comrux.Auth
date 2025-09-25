0c0ecec32a320dcb0914635cd52f4e94dbc63894
Telegrammer <bihug95@gmail.com>
2025-09-25 16:20:23 +0300

    Set notifier to use ne message model

--- src/infrastructure/adapters/notifiers/sensetive_data_change.py
+++ src/infrastructure/adapters/notifiers/sensetive_data_change.py
@@ -1,17 +1,7 @@
-
-
-from dataclasses import dataclass
-
 from application.ports import SensetiveDataChangeNotifier, SensetiveDataChangePayload
 from faststream.redis import RedisBroker
-from datetime import datetime
+from infrastructure.models.messages import UserSensetiveDataChange
 
-#TODO Remove payload structure to model module
-@dataclass
-class Payload:
-    user_id: str
-    changed_data_types: list[str]
-    occured: datetime
 
 class RedisStreamsSensetiveDataChangeNotifier(SensetiveDataChangeNotifier):
 
@@ -19,11 +9,7 @@ class RedisStreamsSensetiveDataChangeNotifier(SensetiveDataChangeNotifier):
         self._broker: RedisBroker = broker
 
     async def notify(self, payload: SensetiveDataChangePayload):
-        
-        #TODO: make payload construction in another object
-        redis_payload: dict[str, str | datetime] = {
-            "user_id": payload.user_id,
-            "changed_fields": payload.changed_data_types,
-            "occured": payload.occured
-        }
-        await self._broker.publish(redis_payload, stream="user:sensetive_data_changed")
+        await self._broker.publish(
+            UserSensetiveDataChange(**payload.__dict__),
+            stream="user:sensetive_data_changed",
+        )
