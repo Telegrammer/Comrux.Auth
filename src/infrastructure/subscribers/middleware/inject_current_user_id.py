fc4e4b36adef6666c3707d1261521206686d2af7
Telegrammer <bihug95@gmail.com>
2025-09-25 16:21:59 +0300

    Encapsulate update context in outer function

--- src/infrastructure/subscribers/middleware/inject_current_user_id.py
+++ src/infrastructure/subscribers/middleware/inject_current_user_id.py
@@ -1,10 +1,8 @@
 __all__ = ["InjectCurrentUserIdMiddleware"]
 
 
-from dishka import AsyncContainer
-from faststream import BaseMiddleware, context
+from faststream import BaseMiddleware
 from faststream.types import DecodedMessage
-from utils.merge_context import merge_context
 from domain import UserId
 from .update_context import update_context
 class InjectCurrentUserIdMiddleware(BaseMiddleware):
@@ -14,9 +12,5 @@ class InjectCurrentUserIdMiddleware(BaseMiddleware):
 
     async def on_consume(self, msg: DecodedMessage) -> DecodedMessage:
         user_id: UserId = UserId(msg.__dict__.get("_decoded_body").get("user_id"))
-
-        old_container: AsyncContainer = context.get("dishka")
-        app_conatiner: AsyncContainer = old_container.parent_container
-        async with app_conatiner(context=merge_context(old_container, {UserId: user_id})) as new_container:
-            context.set_local("dishka", new_container)
+        await update_context({UserId: user_id})
         return msg
--- src/infrastructure/subscribers/middleware/update_context.py
+++ src/infrastructure/subscribers/middleware/update_context.py
@@ -6,7 +6,7 @@ from utils.merge_context import merge_context
 
 
 async def update_context(new_context: dict[Type, object]) -> None:
-    old_container: AsyncContainer = context.get_local("dishka")
+    old_container: AsyncContainer = context.get("dishka")
     app_container: AsyncContainer = old_container.parent_container
     async with app_container(context=merge_context(old_container, new_context)) as new_container:
         context.set_local("dishka", new_container)
