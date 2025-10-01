583b1a2e61e7ee787622dd2918d07511a73870a3
Telegrammer <bihug95@gmail.com>
2025-10-01 15:59:32 +0300

    Define new Uuid7 value object

--- src/domain/value_objects/id.py
+++ src/domain/value_objects/id.py
@@ -1,11 +1,13 @@
 import re
 
 from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
 from .base import ValueObject
 from ..exceptions import DomainFieldError
 
 
-__all__ = ["Id", "Uuid4"]
+__all__ = ["Id", "Uuid4", "Uuid7"]
 
 
 @dataclass(eq=False)
@@ -22,5 +24,19 @@ class Uuid4(Id[str]):
         pattern = re.compile(
             r"^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
         )
+        if not pattern.search(self.value):
+            raise DomainFieldError("value is not an id")
+
+
+@dataclass
+class Uuid7(Id[str]):
+
+    @property
+    def issued_at(self) -> datetime:
+        return datetime.fromtimestamp(UUID(self.value).int >> 12 / 1000)
+
+    def __post_init__(self):
+        super().__post_init__()
+        pattern = re.compile(r"^[0-9a-f]{8}(?:\-[0-9a-f]{4}){3}-[0-9a-f]{12}$")
         if not pattern.search(self.value):
             raise DomainFieldError("value is not an id")
